a0aed7d05cc132c651f52f5ddf0051d25119b1b7
Plasticc66 <xopxe9987@yandex.ru>
2024-02-07 08:14:44 +0300

    step 4 end, modified app with option and params

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,10 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 
 @Command(
         name = "gendiff",
@@ -11,6 +15,15 @@ import picocli.CommandLine.Command;
 )
 
 public class App implements Runnable {
+
+    @Parameters(index = "0", description = "path to first file", defaultValue = "/etc/hosts")
+    private File filepath1 = new File("/etc/hosts");
+    @Parameters(index = "1", description = "path to second file", defaultValue = "/etc/hosts")
+    private File filepath2 = new File("/etc/hosts");
+
+    @Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]")
+    private String format = "stylish";
 
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
