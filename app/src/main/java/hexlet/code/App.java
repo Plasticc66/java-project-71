62848a374eec2bdff17e8df91934e30e98344f04
Ivan Suvorov <98261718+Plasticc66@users.noreply.github.com>
2024-02-07 07:00:48 +0300

    step 3.1 create makefile and write App logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable {
+
+    /*@Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+    private Boolean optionHelp = false;
+
+    @Option(names = {"-v", "--version"}, description = "Print version information and exit.")
+    private Boolean optionVersion = false;*/
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute(args);
     }
+
+    @Override
+    public void run() {
+
+        //todo нужно вывести описание команд, как это сделать - наверное, написано в примере
+    }
+
 }
